0488425e70771ce3816e0de953c3f5f11da6a0fc
Joe V <joevillegasisawesome@gmail.com>
2018-12-23 15:45:21 -0800

    testing code

--- playground.js
+++ playground.js
@@ -1,8 +1,20 @@
-const triggers = require(`./bot_knowledge/phrases/triggers/triggers.json`)
-const phrases_sing = require(`./bot_knowledge/phrases/phrases_sing.json`)
+/*
+//Import tests
+//ES6
+import {
+    songs_to_sing
+} from `./bot_knowledge/phrases/phrases_sing.json`;
 
-
-phrases_sing.songs_to_sing.forEach(thing => {
+songs_to_sing.forEach(thing => {
     console.log(thing.title);
 });
-console.log(triggers.singing_triggers.forEach())
+
+
+//ES5
+
+const triggers = require(`./bot_knowledge/phrases/triggers/triggers.json`)
+
+triggers.singing_triggers.forEach(thing => {
+    console.log(thing.title);
+});
+*/

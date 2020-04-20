0fd43f8ab5a5d0ed77dd35b094db0302eb9c6ebc
Joe V <joexvillegas@gmail.com>
2020-04-20 01:57:27 -0700

    Fix for auto-ban LOL

--- src/bot_functions/novelty/swear/WhitelistFunctions.ts
+++ src/bot_functions/novelty/swear/WhitelistFunctions.ts
@@ -5,11 +5,11 @@ import { you } from './../../../user_creds.json'
 export default class BotModuleSwearWhitelist {
 
     static checkIfWordWhitelistedForRole(word: string, user: Discord.GuildMember) {
-        for (let roleId of Object.keys(you.word_list.white_list))
-            if (user.roles.cache.get(roleId))
-                for (let w of you.word_list.white_list[`${roleId}`])
+        for (let roleName of Object.keys(you.word_list.white_list))
+            if (user.roles.cache.find(r => r.name.includes(roleName)))
+                for (let w of you.word_list.white_list[`${roleName}`])
                     if (word == w) {
-                        console.log(`Word ${word} passes whitelist for role ${roleId} for user ${user.user.username}.`)
+                        console.log(`Word ${word} passes whitelist for role ${roleName} for user ${user.user.username}.`)
                         return true
                     }
     }

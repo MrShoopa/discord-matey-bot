94ab88734ea805cb8b69213cc1c576a83aafd4fb
Joe V <joexvillegas@gmail.com>
2020-05-21 15:53:54 -0700

    WIP Binary

---
+++ src/bot_functions/language/BinaryFunctions.ts
@@ -0,0 +1,10 @@
+import Discord from 'discord.js'
+import Bot from './../../Bot'
+
+import { translate } from '../../bot_knowledge/triggers/triggers.json'
+
+import Binary from 'decode-encode-binary'
+
+export default class BinaryCoderFunctions {
+
+}

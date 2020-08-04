f29e999ed0daa45c5e57a57c9a0abfe554eaec94
Joe Villegas <joexvillegas@gmail.com>
2020-08-03 23:35:59 -0700

    Base subscription class and first plugin

--- src/bot_functions/_state/SubscriptionHandler.ts
+++ src/bot_functions/_state/SubscriptionHandler.ts
@@ -1,14 +1,52 @@
+import Bot from '../../Bot';
+import Discord from 'discord.js'
+
 import { Subscriptions } from './../../types/index';
+import BotGeneralCommands from './../general/GeneralCommands'
 
 export default class SubscriptionHandler {
     static RunTask(subscription: Subscriptions.Subscription) {
+        let bot: Bot = globalThis.Bot
 
+        let subscribedChannelId: string
 
         if (subscription instanceof Subscriptions.ChannelSubscription)
-            SubscriptionHandler.RunChannelTask(subscription as Subscriptions.ChannelSubscription)
+            subscribedChannelId = subscription.channelId
+        else if (subscription instanceof Subscriptions.DMSubscription)
+            subscribedChannelId = subscription.dmChannelId
+        else if (subscription instanceof Subscriptions.UserSubscription)
+            subscribedChannelId = subscription.userId
+
+        let channel: Discord.TextChannel = bot.channels.cache.get(subscribedChannelId) as Discord.TextChannel
+        switch (subscription.featureCode) {
+            case 'Ping':
+                BotGeneralCommands.firePingPongMessage(channel)
+                break;
+            case 'DayCounter':
+                //TODO
+                break;
+            case 'QuoteFetch':
+                //TODO
+                break;
+            case 'RedditPost':
+                //TODO
+                break;
+            default:
+                break;
+        }
+
+        
     }
 
     static RunChannelTask(subscription: Subscriptions.ChannelSubscription) {
 
     }
+    static RunUserTask(subscription: Subscriptions.UserSubscription) {
+
+    }
+    static RunDMTask(subscription: Subscriptions.DMSubscription) {
+
+    }
+
+
 }
--- src/types/data_types/Subscription.ts
+++ src/types/data_types/Subscription.ts
@@ -1,3 +1,5 @@
+import Discord from 'discord.js'
+
 export module Subscriptions {
     export interface Subscription extends Object {
         featureCode: SubscriptionFeature,
@@ -6,22 +8,49 @@ export module Subscriptions {
         endDate?: Date,
         [args: string]: any
     }
-    export interface ChannelSubscription extends Subscription {
-        channelId: string,
-        authorId: string
-    }
-
-    export interface UserSubscription extends Subscription {
-        userId: string
-    }
-
-    export interface DMSubscription extends Subscription {
+    export class ChannelSubscription implements Subscription {
+        frequencyMilli: Number
         channelId: string
+        authorId: string
+        featureCode: SubscriptionFeature
+        [arg: string]: any
+
+        constructor(callingUser: Discord.GuildMember, featureCode: SubscriptionFeature, frequencyMilli: Number, args?: any) {
+            this.authorId = callingUser.id
+            this.channelId = callingUser.lastMessageChannelID
+            this.featureCode = featureCode
+            this.frequencyMilli = frequencyMilli
+        }
     }
 
-    export enum SubscriptionFeature {
-        Ping,
-        DayCounter,
-        RedditPost
+    export class UserSubscription implements Subscription {
+        userId: string
+        featureCode: SubscriptionFeature
+        frequencyMilli: Number
+        [arg: string]: any
+
+        constructor(callingUser: Discord.User, featureCode: SubscriptionFeature, args?: any) {
+            this.userId = callingUser.id
+            this.featureCode = featureCode
+            this.frequencyMilli
+        }
     }
+
+    export class DMSubscription implements Subscription {
+        [args: string]: any
+        dmChannelId: string
+        featureCode: SubscriptionFeature
+        frequencyMilli: Number
+        durationMilli?: Number
+        endDate?: Date
+
+        constructor(callingDM: Discord.DMChannel, featureCode: SubscriptionFeature, args?: any) {
+            this.dmChannelId = callingDM.id
+            this.featureCode = featureCode
+            this.frequencyMilli = this.frequencyMilli
+        }
+    }
+
+    export type SubscriptionFeature = 'Ping' | 'DayCounter' | 'QuoteFetch' | 'RedditPost'
+
 }

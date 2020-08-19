a0b52f04d1ef195a2a000b9cffea3918b1eb7857
Joe Villegas <joexvillegas@gmail.com>
2020-08-18 22:54:49 -0700

    Add periodical inspirational quote

--- src/bot_functions/_state/DiscordActivityStatus.ts
+++ src/bot_functions/_state/DiscordActivityStatus.ts
@@ -1,5 +1,6 @@
 import Bot from "../../Bot"
 import { ActivityOptions } from "discord.js"
+import BotModuleQuote from "../fetching/quote/QuoteFunctions"
 
 export default class BotDiscordActivity {
     public static customStatuses: Array<ActivityOptions> = [
@@ -51,28 +52,29 @@ export default class BotDiscordActivity {
         { name: `crippling ${globalThis.bot.guilds.cache.size} servers`, type: "STREAMING" },
         { name: `loves his ${globalThis.bot.users.cache.size} peeps`, type: "STREAMING" },
         { name: `pattycake with ${globalThis.bot.guilds.cache.size} servers`, type: 'PLAYING' },
-        { name: `${globalThis.bot.guilds.cache.size} zooms`, type: 'LISTENING' }
+        { name: `${globalThis.bot.guilds.cache.size} zooms`, type: 'LISTENING' },
+        { name: `${BotModuleQuote.fetchInspirationalQuote().then(m => m.title)} zooms`, type: 'LISTENING' }
     ]
 
-    static getRandomStatus(): ActivityOptions {
+    static async getRandomStatus(): Promise<ActivityOptions> {
         let looklist = this.customStatuses
 
         if (globalThis.bot)
-            looklist = this.customStatuses.concat(new BotDiscordActivity().dynamicStatuses)
+            looklist = this.customStatuses.concat(await new BotDiscordActivity().dynamicStatuses)
 
         let dice = Math.floor(Math.random() * looklist.length)
 
         return looklist[dice]
     }
 
-    static updateRandomStatus() {
+    static async updateRandomStatus() {
         let bot: Bot = globalThis.bot
 
         bot.voice.connections.size
 
         bot.user.setUsername("Megadork");
 
-        bot.user.setActivity(BotDiscordActivity.getRandomStatus())
+        bot.user.setActivity(await BotDiscordActivity.getRandomStatus())
     }
 
     static useDevModeStatus() {
